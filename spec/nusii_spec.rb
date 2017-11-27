cabf032f818ff49ef3556723d6835f1cf59f9842
Victor <victor.martin84@gmail.com>
2017-11-27 20:45:16 +0100

    api key spec

--- spec/nusii_spec.rb
+++ spec/nusii_spec.rb
@@ -1,7 +1,19 @@
 require "spec_helper"
 
 RSpec.describe Nusii do
-  it "has a version number" do
-    expect(Nusii::VERSION).not_to be nil
+  describe 'version number' do
+    it "has a version number" do
+      expect(Nusii::VERSION).not_to be nil
+    end
+  end
+
+  describe 'api_key' do
+    let(:api_key) { 'thisismyapikey' }
+
+    it "has an api_key" do
+      expect(Nusii.api_key).to be_nil
+      Nusii.api_key = api_key
+      expect(Nusii.api_key).to eq api_key
+    end
   end
 end
