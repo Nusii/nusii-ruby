fe252f45c270af6e1e696c63b3da3da9e8af3fa5
Victor <victor.martin84@gmail.com>
2017-12-04 17:04:12 +0100

    Client list method

--- lib/nusii/client.rb
+++ lib/nusii/client.rb
@@ -11,6 +11,10 @@ module Nusii
       requester.show_call self, id
     end
 
+    def self.list options={}
+      requester = Nusii::Request.new
+      requester.index_call self, options
+    end
 
   end
 end
