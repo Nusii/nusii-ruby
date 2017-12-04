192d272a682765cec440ad73b2b1fc3a3a05975c
Victor <victor.martin84@gmail.com>
2017-12-04 16:16:40 +0100

    Request#show may receive options parameter that will pass along to the api_operation method

--- lib/nusii/request.rb
+++ lib/nusii/request.rb
@@ -1,8 +1,8 @@
 module Nusii
   class Request
 
-    def show_call resource_class, resource_id = nil
-      operator = ApiOperations::Show.new(resource_class, resource_id)
+    def show_call resource_class, resource_id = nil, options = {}
+      operator = ApiOperations::Show.new(resource_class, resource_id, options)
       operator.call
     end
 
