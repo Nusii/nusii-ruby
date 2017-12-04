c7318619c5fc7c807e41413f4475cef8b4dd36fb
Victor <victor.martin84@gmail.com>
2017-12-04 17:35:12 +0100

    .list method for proposals

--- lib/nusii/proposal.rb
+++ lib/nusii/proposal.rb
@@ -10,5 +10,10 @@ module Nusii
       requester.show_call self, id, options
     end
 
+    def self.list options={}
+      requester = Nusii::Request.new
+      requester.index_call self, options
+    end
+
   end
 end
