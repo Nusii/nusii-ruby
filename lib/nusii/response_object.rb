bb49c0cc8f5f6553862507f6fc553262c61154c1
Victor <victor.martin84@gmail.com>
2017-12-04 17:04:25 +0100

    ResponseObject

---
+++ lib/nusii/response_object.rb
@@ -0,0 +1,23 @@
+module Nusii
+  class ResponseObject
+
+    attr_accessor :resources, :meta, :resource_class
+
+    def initialize resources, meta, resource_class
+      @resources      = resources
+      @meta           = meta
+      @resource_class = resource_class
+    end
+
+    def next_page
+      raise(ArgumentError, "That page does no exist") if meta['next_page'] == nil
+      resource_class.list(:page => meta['next_page'])
+    end
+
+    def prev_page
+      raise(ArgumentError, "That page does no exist") if meta['prev_page'] == nil
+      resource_class.list(:page => meta['prev_page'])
+    end
+
+  end
+end
