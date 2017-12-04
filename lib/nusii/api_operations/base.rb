4b951b84e44df5ddcfeea03ef54279d2229c25d3
Victor <victor.martin84@gmail.com>
2017-12-04 12:53:07 +0100

    ApiOperations::Base and ApiOperations::Connection classes.

---
+++ lib/nusii/api_operations/base.rb
@@ -0,0 +1,60 @@
+module Nusii
+  module ApiOperations
+    class Base
+
+      attr_reader :resource_class, :resource_id
+
+      def initialize resource_class, resource_id = nil
+        @resource_class = resource_class
+        @resource_id    = resource_id
+      end
+
+      def call
+        if response.status == 200
+          build_ok_response
+        else
+          raise_error_response
+        end
+      end
+
+    private
+
+      def response
+        raise NotImplementedError,
+          "Each subclass must implement this method"
+      end
+
+      def build_ok_response
+        raise NotImplementedError,
+          "Each subclass must implement this method"
+      end
+
+      def resource_path
+        raise NotImplementedError,
+          "Each subclass must implement this method"
+      end
+
+      def raise_error_response
+        current_error = NusiiError.error_for(status)
+
+        raise current_error.new(status, body, reason_phrase)
+      end
+
+      def parsed_body
+        @parsed_body ||= JSON.parse body
+      end
+
+      def resource_url_string
+        resource_class.to_s.split('::').last.downcase.pluralize
+      end
+
+      def connection
+        @connection ||= Connection.new
+      end
+
+      delegate :status, :body, :reason_phrase,
+               :to => :response
+
+    end
+  end
+end
---
+++ lib/nusii/api_operations/connection.rb
@@ -0,0 +1,21 @@
+module Nusii
+  class Connection
+
+    delegate :get, :post, :put, :delete,
+      :to => :connection
+
+    HOST = "https://app.nusii.com"
+
+  private
+
+    def connection
+      @connection ||= Faraday.new(HOST) do |conn|
+        conn.adapter :net_http
+        conn.headers['Authorization'] = "Token token=#{Nusii.api_key}"
+        conn.headers['User-Agent']     = Nusii.user_agent
+        conn.headers['Content-Type']  = 'application/vnd.api+json'
+      end
+    end
+
+  end
+end
