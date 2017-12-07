457a35cb2bfdc880103b07a73f2ad1a2dc010c97
Victor <victor.martin84@gmail.com>
2017-12-07 13:09:06 +0100

    Adding webmock and Fake Sinatra app

--- nusii.gemspec
+++ nusii.gemspec
@@ -28,4 +28,6 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
+  spec.add_development_dependency "webmock", "~> 3.1.1"
+  spec.add_development_dependency "sinatra", "~> 2.0.0"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,18 @@
 require "bundler/setup"
 require "nusii"
+require 'webmock/rspec'
+require 'support/fake_nusii'
+
+WebMock.disable_net_connect!(allow_localhost: true)
 
 RSpec.configure do |config|
   config.example_status_persistence_file_path = ".rspec_status"
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
+  end
+
+  config.before(:each) do
+    stub_request(:any, /app.nusii.com/).to_rack(FakeNusii)
   end
 end
