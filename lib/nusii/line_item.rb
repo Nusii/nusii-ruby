8acd8a7994f76f3f5be9eb93851a069c650f750b
Victor <victor.martin84@gmail.com>
2017-12-07 16:35:04 +0100

    Create with section

--- lib/nusii/line_item.rb
+++ lib/nusii/line_item.rb
@@ -1,8 +1,6 @@
 module Nusii
   class LineItem < Resource
     extend Nusii::Crud::List
-    extend Nusii::Crud::Create
-    include Nusii::Crud::Save
     extend Nusii::Crud::Destroy
     include Nusii::Crud::DestroySelf
 
@@ -14,6 +12,18 @@ module Nusii
     def self.list_by_section section_id, options={}
       requester = Nusii::Request.new
       requester.nested_index_call self, section_id, Nusii::Section, options
+    end
+
+    def self.create_with_section section_id, params
+      requester = Nusii::Request.new
+      requester.nested_create_call self, section_id, Nusii::Section, params
+    end
+
+    def save
+      raise(ArgumentError, 'You can\'t update a line item without id') if self.id.blank?
+
+      requester = Nusii::Request.new
+      requester.update_call self.class, self
     end
 
   private
--- spec/nusii/line_item_spec.rb
+++ spec/nusii/line_item_spec.rb
@@ -20,4 +20,22 @@ RSpec.describe Nusii::LineItem do
     end
   end
 
+  describe '.create_with_section' do
+    let(:response_object) { double 'response_object' }
+    let(:params) { {} }
+    let(:section_id) { 54 }
+    let(:requester) { double 'requester' }
+
+    it 'relies on the right class' do
+      expect(Nusii::Request).to receive(:new).
+        and_return requester
+
+      expect(requester).to receive(:nested_create_call).
+        with(described_class, section_id, Nusii::Section, params).
+        and_return response_object
+
+      expect(described_class.create_with_section(section_id, params)).to eq response_object
+    end
+  end
+
 end
